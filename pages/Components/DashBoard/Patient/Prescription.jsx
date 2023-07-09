b65dac3a4483e17caf1fb69bc2d0e70caaacd038
Your Name <youremail@yourdomain.com>
2023-07-09 16:50:58 +0600

    done

--- pages/Components/DashBoard/Patient/Prescription.jsx
+++ pages/Components/DashBoard/Patient/Prescription.jsx
@@ -1,35 +1,25 @@
 import React from 'react'
 import FileUpload from '../../File/FileUpload'
 import DisplayFile from '../../File/DisplayFile'
-import 'reactjs-popup/dist/index.css';
-import Styles from './Prescription.module.css';
-import Popup from 'reactjs-popup';
+
+
+
 import BoxButton from '../../BoxButton';
 const Prescription = () => {
     return (
         <div>
 
-            <Popup
-                trigger={<button className={Styles.button}>
-                    <BoxButton name="Upload File" handleFunction={() => { }} />
-                </button>}
-                modal
-                nested
-            >
-                {close => (
-                    <div className={Styles.modal}>
-                        <button className={Styles.close} onClick={close}>
-                            &times;
-                        </button>
 
-                        <div className={Styles.content}>
-                            <FileUpload />
-                            <DisplayFile />
-                        </div>
+            <BoxButton name="Upload File" />
 
-                    </div>
-                )}
-            </Popup>
+
+
+            <div >
+                <FileUpload />
+                <DisplayFile />
+            </div>
+
+
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 // import { NavBar } from '@/components'
-import {React, useEffect} from 'react'
+import { React, useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import HealthProvider from '@/context/Health'
 import '@/styles/globals.css'
-import {DevSupport} from "@react-buddy/ide-toolbox-next";
-import {ComponentPreviews, useInitial} from "@/components/dev";
 
-const App = ({Component, pageProps}) => {
+
+const App = ({ Component, pageProps }) => {
     useEffect(() => {
         AOS.init({
             duration: 800,
@@ -16,11 +15,9 @@ const App = ({Component, pageProps}) => {
     }, [])
     return (<HealthProvider>
         {/* <NavBar/> */}
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <Component {...pageProps} />
-        </DevSupport>
+
+        <Component {...pageProps} />
+
     </HealthProvider>)
 }
 
