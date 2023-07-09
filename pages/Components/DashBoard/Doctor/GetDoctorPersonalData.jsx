3cc941931e34bc5b0c15de7b2290c21a7ba8ab7a
Your Name <youremail@yourdomain.com>
2023-07-09 16:28:32 +0600

    done

--- pages/Components/DashBoard/Doctor/GetDoctorPersonalData.jsx
+++ pages/Components/DashBoard/Doctor/GetDoctorPersonalData.jsx
@@ -12,19 +12,15 @@ const GetDoctorPersonalData = () => {
         <div>
             <div>
 
-                <button type="" class="m-xl-5 align-content-center justify-content-center  d-flex align-content-sm-center" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
+                <button >
                     <BoxButton name=" Doctor Personal Information" handleFunction={doctorAllData} />
                 </button>
 
 
 
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title " id="exampleModalLabel">Doctor Personal Information</h5>
-                                <button type="button" class="btn-close mx-3" data-bs-dismiss="modal" aria-label="Close">Close</button>
-                            </div>
+                <div >
+
+
                             <div class="modal-body">
                                 {console.log(doctorData)}
                                 Account: {doctorData[0]}
@@ -41,18 +37,14 @@ const GetDoctorPersonalData = () => {
                                 <hr />
 
                                 Year Of Experience: {String(doctorData[6])}
-                                <div class="modal-footer">
-                                    <button type="btn btn-dark" className="btn btn-dark" data-bs-dismiss="modal">Close</button>
-                                    {/* <button type="submit btn btn-primary" onClick={() => { handleSubmit }} class="btn btn-primary">Submit</button> */}
-                                </div>
-                            </div>
 
-                        </div>
-                    </div>
+
+
                 </div>
 
 
 
+            </div>
             </div>
         </div>
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,27 @@
 // import { NavBar } from '@/components'
-import { React, useEffect } from 'react'
+import {React, useEffect} from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import HealthProvider from '@/context/Health'
 import '@/styles/globals.css'
+import {DevSupport} from "@react-buddy/ide-toolbox-next";
+import {ComponentPreviews, useInitial} from "@/components/dev";
 
-const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    AOS.init({
-      duration: 800,
-      once: false,
-    })
-  }, [])
-  return (<HealthProvider>
-    {/* <NavBar/> */}
-    <Component {...pageProps} />
-  </HealthProvider>)
+const App = ({Component, pageProps}) => {
+    useEffect(() => {
+        AOS.init({
+            duration: 800,
+            once: false,
+        })
+    }, [])
+    return (<HealthProvider>
+        {/* <NavBar/> */}
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <Component {...pageProps} />
+        </DevSupport>
+    </HealthProvider>)
 }
 
 export default App
