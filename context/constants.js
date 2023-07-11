fb77a9464ae8e0e6122a7c8946ef5552ce34e47c
Your Name <youremail@yourdomain.com>
2023-07-11 19:20:40 +0600

    done

--- context/constants.js
+++ context/constants.js
@@ -1,7 +1,7 @@
 import health from "./MedicalData.json";
 import patient from "./MedicalData.json";
 
-export const healthAddess = "0x57D8024C5089bd8C4f9FF15618e8FBb82948A892";
+export const healthAddess = "0x3645f422969851Fea2A4E85A22f1B0eC7b97E4Dc";
 export const patientAddress = healthAddess;
 export const healthABI = health.abi;
 export const patientABI = patient.abi;
