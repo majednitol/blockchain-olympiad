574f99051b5f315a50f5d754022d306b90ba7434
Your Name <youremail@yourdomain.com>
2023-07-13 09:49:02 +0600

    done

--- pages/Components/DashBoard/Patient/Patient.jsx
+++ pages/Components/DashBoard/Patient/Patient.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import GetPersonalDetails from "./GetPersonalDetails";
 import Prescription from "./Prescription";
 import DisplayFile from "../../File/DisplayFile";
+import SetPersonalHealthData from "@/pages/Components/DashBoard/Patient/SetPersonalHealthData";
 
 const PatientComponent = () => {
   return (
     <div>
       <GetPersonalDetails />
+      <SetPersonalHealthData />
       <Prescription />
       <DisplayFile />
     </div>
